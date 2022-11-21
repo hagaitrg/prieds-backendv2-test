97406e8a1d029b2b6e630592679cb9b7413b6610
hagaitrg <disahagait@gmail.com>
2022-11-22 01:48:22 +0700

    setup mongodb

---
+++ mongodb.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+require('dotenv').config();
+
+var url = process.env.DB_URL;
+
+var DB_ref = mongoose
+  .createConnection( url )
+
+  .on('error', function (err) {
+    if (err) {
+      console.error('Error connecting to MongoDB.', err.message);
+      process.exit(1);
+    }
+  })
+  .once('open', function callback() {
+    console.info('Mongo db connected successfully ' + db);
+  });
+
+module.exports = DB_ref;
